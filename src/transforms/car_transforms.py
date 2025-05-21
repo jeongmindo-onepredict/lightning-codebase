2c6e434d1989d881afd9bff62de813ac54891aa5
jeongmin <silverstar456@yonsei.ac.kr>
2025-05-21 01:53:15 +0000

    add: eva

--- src/transforms/car_transforms.py
+++ src/transforms/car_transforms.py
@@ -11,7 +11,7 @@ class CarTransforms(BaseTransforms):
     def train_transform(self):
         return transforms.Compose([
             # PIL 이미지에 적용되는 변환
-            transforms.Resize((256, 256)),
+            transforms.Resize((self.img_size, self.img_size)),
             # transforms.RandomResizedCrop(224, scale=(0.6, 1.0)),
             transforms.RandomHorizontalFlip(),
             transforms.RandomRotation(20),
@@ -31,11 +31,7 @@ class CarTransforms(BaseTransforms):
 
     def val_transform(self):
         return transforms.Compose([
-<<<<<<< HEAD
             transforms.Resize((self.img_size, self.img_size)),
-=======
-            transforms.Resize((256, 256)),
->>>>>>> 9a1a5404b2c38fe369c7f5f54397c8b0de88d2fb
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
