8dc7cebb41eb00693047a78609e6bab193044fba
jeongmin <silverstar456@yonsei.ac.kr>
2025-05-23 03:48:07 +0000

    tresnet xl

--- src/transforms/car_transforms.py
+++ src/transforms/car_transforms.py
@@ -10,7 +10,7 @@ class CarTransforms(BaseTransforms):
         return transforms.Compose([
             # PIL 이미지에 적용되는 변환
             transforms.Resize((300, 300)),
-            transforms.RandomResizedCrop(224, scale=(0.6, 1.0)),
+            transforms.RandomResizedCrop(600, scale=(0.6, 1.0)),
             transforms.RandomHorizontalFlip(),
             transforms.RandomRotation(20),
             transforms.RandomAffine(0, translate=(0.2, 0.2), scale=(0.8, 1.2)),
@@ -29,7 +29,7 @@ class CarTransforms(BaseTransforms):
 
     def val_transform(self):
         return transforms.Compose([
-            transforms.Resize((224, 224)),
+            transforms.Resize((600, 600)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
