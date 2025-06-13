8a5453ec6c7c4578dd88bf942ccea9d79560351b
jeongmin <silverstar456@yonsei.ac.kr>
2025-06-14 04:20:02 +0900

    prep

--- scripts/kfold.sh
+++ scripts/kfold.sh
@@ -1,14 +1,14 @@
-python3 src/prep_data.py --dataset ohjoonhee/UsedCarsImageNetGamma --output_dir datasets --split fold1of5
+python3 prep_data.py --dataset ohjoonhee/UsedCarsImageNetGamma --output_dir datasets --split fold1of5
 python3 -u src/main.py fit -c configs/1.yaml $@
 
-python3 src/prep_data.py --dataset ohjoonhee/UsedCarsImageNetGamma --output_dir datasets --split fold2of5
+python3 prep_data.py --dataset ohjoonhee/UsedCarsImageNetGamma --output_dir datasets --split fold2of5
 python3 -u src/main.py fit -c configs/2.yaml $@
 
-python3 src/prep_data.py --dataset ohjoonhee/UsedCarsImageNetGamma --output_dir datasets --split fold3of5
+python3 prep_data.py --dataset ohjoonhee/UsedCarsImageNetGamma --output_dir datasets --split fold3of5
 python3 -u src/main.py fit -c configs/3.yaml $@
 
-python3 src/prep_data.py --dataset ohjoonhee/UsedCarsImageNetGamma --output_dir datasets --split fold4of5
+python3 prep_data.py --dataset ohjoonhee/UsedCarsImageNetGamma --output_dir datasets --split fold4of5
 python3 -u src/main.py fit -c configs/4.yaml $@
 
-python3 src/prep_data.py --dataset ohjoonhee/UsedCarsImageNetGamma --output_dir datasets --split fold5of5
+python3 prep_data.py --dataset ohjoonhee/UsedCarsImageNetGamma --output_dir datasets --split fold5of5
 python3 -u src/main.py fit -c configs/5.yaml $@
