5ca269a73d024dcec9b35688d5c767cb82201722
jeongmin <silverstar456@yonsei.ac.kr>
2025-06-13 19:30:14 +0000

    kfold

--- scripts/kfold.sh
+++ scripts/kfold.sh
@@ -1,14 +1,19 @@
+rm -rf datasets
 python3 prep_data.py --dataset ohjoonhee/UsedCarsImageNetGamma --output_dir datasets --split fold1of5
 python3 -u src/main.py fit -c configs/1.yaml $@
 
+rm -rf datasets
 python3 prep_data.py --dataset ohjoonhee/UsedCarsImageNetGamma --output_dir datasets --split fold2of5
 python3 -u src/main.py fit -c configs/2.yaml $@
 
+rm -rf datasets
 python3 prep_data.py --dataset ohjoonhee/UsedCarsImageNetGamma --output_dir datasets --split fold3of5
 python3 -u src/main.py fit -c configs/3.yaml $@
 
+rm -rf datasets
 python3 prep_data.py --dataset ohjoonhee/UsedCarsImageNetGamma --output_dir datasets --split fold4of5
 python3 -u src/main.py fit -c configs/4.yaml $@
 
+rm -rf datasets
 python3 prep_data.py --dataset ohjoonhee/UsedCarsImageNetGamma --output_dir datasets --split fold5of5
 python3 -u src/main.py fit -c configs/5.yaml $@
